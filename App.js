31d4c037ed1d9119f73864e385792026bce3ba0b
Bulala_Christan <christanbulala33@gmail.com>
2025-09-27 20:58:39 +0800

    Added App.js to MidAct1 branch

---
+++ App.js
@@ -0,0 +1,13 @@
+//App.js
+import {SafeAreaView} from 'react-native'
+import CommentApp from './CommentApp'
+import Messenger from './Messenger'
+
+export default function App(){
+  return(
+    <SafeAreaView style={{flex: 1}}>
+      <Messenger/>
+      <CommentApp/>
+    </SafeAreaView>
+    )
+}
